5c70916625a476e6ecddef51b15c83dadcd1421a
clctvintel-star <clctv.mail@gmail.com>
2026-03-29 16:51:21 -0400

    Update dorian_config.py

--- config/dorian_config.py
+++ config/dorian_config.py
@@ -1,112 +1,88 @@
-from pathlib import Path
-import os
+ci2_root: /content/drive/MyDrive/CI2
 
-# ============================================
-# ROOT
-# ============================================
+paths:
+  dorian_db: /content/drive/MyDrive/CI2/db/dorian2
+  brand_input_dir: /content/drive/MyDrive/CI2/db/dorian2/inputs/brand
+  dorian_runs_dir: /content/drive/MyDrive/CI2/db/dorian2/runs
 
-CI2_ROOT = Path(os.environ.get("CI2_ROOT", "/content/drive/MyDrive/CI2"))
+  qwass_roots:
+    - /content/drive/MyDrive/CI2/db/qwass2
+    - /content/drive/MyDrive/CI2/QWASS
 
-# ============================================
-# DATA PATHS
-# ============================================
+  scum_roots:
+    - /content/drive/MyDrive/CI2/db/scum2
+    - /content/drive/MyDrive/CI2/SCUM
 
-PATHS = {
-    "dorian_db": CI2_ROOT / "db" / "dorian2",
-    "dorian_runs": CI2_ROOT / "db" / "dorian2" / "runs",
-    "dorian_brand": CI2_ROOT / "db" / "dorian2" / "inputs" / "brand",
+  werk_roots:
+    - /content/drive/MyDrive/CI2/db/werk2
+    - /content/drive/MyDrive/CI2/WERK
+    - /content/drive/MyDrive/CI2/WERK/werk.outputs
 
-    "qwass": CI2_ROOT / "db" / "qwass2",
-    "scum": CI2_ROOT / "db" / "scum2",
-    "werk": CI2_ROOT / "db" / "werk2",
+funds:
+  - Citadel
+  - Millennium
+  - Two Sigma
+  - D.E. Shaw
+  - Jane Street
+  - Hudson River Trading
+  - Point72
+  - Balyasny
+  - Schonfeld
+  - ExodusPoint
+  - Jump Trading
 
-    # fallback (legacy)
-    "legacy_qwass": CI2_ROOT / "QWASS",
-    "legacy_scum": CI2_ROOT / "SCUM",
-    "legacy_werk": CI2_ROOT / "WERK",
-}
+fund_path_fix:
+  "Two Sigma": "TwoSigma"
+  "D.E. Shaw": "D.E.Shaw"
+  "Hudson River Trading": "HudsonRiverTrading"
+  "Jane Street": "JaneStreet"
+  "Jump Trading": "JumpTrading"
 
-# ============================================
-# FUNDS
-# ============================================
+fund_aliases:
+  citadel: Citadel
+  millennium: Millennium
+  mlp: Millennium
+  "two sigma": "Two Sigma"
+  twosigma: "Two Sigma"
+  "two-sigma": "Two Sigma"
+  "d.e. shaw": "D.E. Shaw"
+  "de shaw": "D.E. Shaw"
+  deshaw: "D.E. Shaw"
+  "jane street": "Jane Street"
+  janestreet: "Jane Street"
+  "jane.street": "Jane Street"
+  "hudson river trading": "Hudson River Trading"
+  hrt: "Hudson River Trading"
+  point72: Point72
+  balyasny: Balyasny
+  schonfeld: Schonfeld
+  exoduspoint: ExodusPoint
+  "jump trading": "Jump Trading"
 
-CANON_FUNDS = [
-    "Citadel",
-    "Millennium",
-    "Two Sigma",
-    "D.E. Shaw",
-    "Jane Street",
-    "Hudson River Trading",
-    "Point72",
-    "Balyasny",
-    "Schonfeld",
-    "ExodusPoint",
-    "Jump Trading",
-]
+scum_package_dir:
+  Citadel: citadel.scum.package
+  Millennium: millennium.scum.package
+  "Two Sigma": twosigma.scum.package
+  "D.E. Shaw": deshaw.scum.package
+  "Jane Street": janestreet.scum.package
+  "Hudson River Trading": hudsonrivertrading.scum.package
+  Point72: point72.scum.package
+  Balyasny: balyasny.scum.package
+  Schonfeld: schonfeld.scum.package
+  ExodusPoint: exoduspoint.scum.package
+  "Jump Trading": jumptrading.scum.package
 
-FUND_PATH_FIX = {
-    "Two Sigma": "TwoSigma",
-    "D.E. Shaw": "D.E.Shaw",
-    "Hudson River Trading": "HudsonRiverTrading",
-    "Jane Street": "JaneStreet",
-    "Jump Trading": "JumpTrading",
-}
+chunking:
+  max_words: 180
+  overlap_words: 40
+  min_chunk_words: 25
 
-FUND_ALIASES = {
-    "citadel": "Citadel",
-    "millennium": "Millennium",
-    "mlp": "Millennium",
-    "two sigma": "Two Sigma",
-    "twosigma": "Two Sigma",
-    "two-sigma": "Two Sigma",
-    "de shaw": "D.E. Shaw",
-    "d.e. shaw": "D.E. Shaw",
-    "deshaw": "D.E. Shaw",
-    "jane street": "Jane Street",
-    "janestreet": "Jane Street",
-    "jane.street": "Jane Street",
-    "hrt": "Hudson River Trading",
-    "hudson river trading": "Hudson River Trading",
-    "point72": "Point72",
-    "balyasny": "Balyasny",
-    "schonfeld": "Schonfeld",
-    "exoduspoint": "ExodusPoint",
-    "jump trading": "Jump Trading",
-}
+filters:
+  reputation_months: 18
+  news_quality_cutoff: 0.65
 
-SCUM_PACKAGE_DIR = {
-    "Citadel": "citadel.scum.package",
-    "Millennium": "millennium.scum.package",
-    "Two Sigma": "twosigma.scum.package",
-    "D.E. Shaw": "deshaw.scum.package",
-    "Hudson River Trading": "hudsonrivertrading.scum.package",
-    "Jane Street": "janestreet.scum.package",
-    "Point72": "point72.scum.package",
-    "Balyasny": "balyasny.scum.package",
-    "Schonfeld": "schonfeld.scum.package",
-    "ExodusPoint": "exoduspoint.scum.package",
-    "Jump Trading": "jumptrading.scum.package",
-}
-
-# ============================================
-# PIPELINE SETTINGS
-# ============================================
-
-CHUNKING = {
-    "max_words": 180,
-    "overlap_words": 40,
-    "min_chunk_words": 25,
-}
-
-FILTERS = {
-    "reputation_months": 18,
-    "news_quality_cutoff": 0.65,
-}
-
-WEIGHTS = {
-    "source_base": {
-        "news": 1.00,
-        "reddit": 0.85,
-        "glassdoor": 0.95,
-    }
-}
+weights:
+  source_base:
+    news: 1.00
+    reddit: 0.85
+    glassdoor: 0.95
